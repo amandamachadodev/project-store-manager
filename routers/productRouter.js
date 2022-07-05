6b9c5b29be1f31dcf694e6204f1b88c1d55f1ee8
mandioquynha <machado.amandag@gmail.com>
2022-07-05 18:50:34 -0300

    req4 em andamento

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -1,12 +1,15 @@
 const { Router } = require('express');
+
 const products = require('../controllers/products');
+
+const isValidName = require('../middlewares/validateName');
 
 const productsRoute = Router();
 
-productsRoute.get('/', products.listAll);
+productsRoute.get('/', isValidName, products.listAll);
 
 productsRoute.get('/:id', products.findId);
 
-productsRoute.post('/', products.create);
+productsRoute.post('/', isValidName, products.create);
 
 module.exports = productsRoute;
